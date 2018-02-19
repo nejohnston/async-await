6c9d5a789e45d811c285a85447bb2c339fe9e36b
Nicholas Johnston <nicholas.johnston94@gmail.com>
2018-02-19 13:36:12 -0800

    refactored so fResult was defined

--- async-await.js
+++ async-await.js
@@ -1,23 +1,26 @@
 function mockAsyncSlowAdder(number1, number2) {
-    return new Promise((resolve, reject) => {
-      console.log("Slowly adding...", number1, "+", number2);
-  
-      setTimeout(() => {
-        const added = number1 + number2;
-        if (!added) return reject("invalid addition..")
-        resolve(added);
-      }, 2000);
-    });
-  }
+  return new Promise((resolve, reject) => {
+    console.log("Slowly adding...", number1, "+", number2);
 
-  
+    setTimeout(() => {
+      const added = number1 + number2;
+      if (!added) return reject("invalid addition..");
+      resolve(added);
+    }, 2000);
+  });
+}
+
+const addEm = async () => {
   try {
-    const result1 = await mockAsyncSlowAdder(1, 2)
-    const result2 = await mockAsyncSlowAdder(1, result1)
-    const result3 = await mockAsyncSlowAdder(1, result2)
-  } catch(e) {
-      console.log(e)
+    const result1 = await mockAsyncSlowAdder(1, 2);
+    const result2 = await mockAsyncSlowAdder(1, result1);
+    const fResult = await mockAsyncSlowAdder(1, result2);
+    return fResult;
+  } catch (e) {
+    console.log(e);
   }
+};
 
-
-  console.log('Awaited! Done!', finalResult)
+addEm().then(result => {
+  console.log("Awaited! Done!", result);
+});
