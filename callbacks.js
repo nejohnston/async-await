edfb1d1860170a53bf1d95bcf2a5500a6a74f062
Nicholas Johnston <nicholas.johnston94@gmail.com>
2018-02-19 11:54:39 -0800

    added callback

--- callbacks.js
+++ callbacks.js
@@ -1,7 +1,12 @@
-function mockAsync(data, done) {
+function mockAsyncSlowAdder(number1, number2, done) {
+  console.log("Slowly adding...", number1, "+", number2);
   setTimeout(() => {
-    done(`Im done processing this IO ${data}`);
+    const added = number1 + number2;
+    done(null, added);
   }, 2000);
 }
 
 // Basic Callback Pattern
+mockAsyncSlowAdder(1, 2, (err, result) => {
+  console.log("Done!", result);
+});
